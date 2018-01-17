ba35b7073a73fb74b2a86530ca5d38f51c97a30c
djseanizellkenya@gmail.com <djseanizellkenya@gmail.com>
2018-01-17 12:02:09 +0300

    added client Id and secret

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Http,Headers} from '@angular/http';
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProfileService {
 
-  constructor() { }
+private username :string;
+private clientid = "a19c02dde06db7f06299";
+private clientsecret ="4141834f531c4182977b302df2c7225af549673e" ; 
+
+  constructor(private http:Http) { }
 
 }

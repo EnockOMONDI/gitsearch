05fc2586f48b32a77cb9eb6e2f817b14d048bbd4
djseanizellkenya@gmail.com <djseanizellkenya@gmail.com>
2018-01-17 12:18:39 +0300

    added my profile

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,8 +7,13 @@ export class ProfileService {
 
 private username :string;
 private clientid = "a19c02dde06db7f06299";
-private clientsecret ="4141834f531c4182977b302df2c7225af549673e" ; 
+private clientsecret ="4141834f531c4182977b302df2c7225af549673e" ;
 
   constructor(private http:Http) { }
+console.log("service is now ready!");
+this.username = "enockomondi"
+}
 
+getProfileinfo(){
+  return this.http.get("https://api.github.com/users/enockomondi" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
 }
